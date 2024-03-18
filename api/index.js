58e10f7c2b3c57cfaf9b47911ceb716833f12501
Alan Zacharia <alanzacaharia@gmail.com>
2024-03-18 11:11:54 +0530

    mongo-db-connected

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 const PORT = 3000;
 
+// Connect to MongoDB database.
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => console.log("Connected to the Database!"))
+    .catch((err) => console.error(err));
+
+app.use(express.json());
 
 
 
